apache/nuttx

sim: fix sim runtime err under sanitize check mode.
since gcc sanitize can not stub proper code in nuttx kernel code.

Change-Id: Ibceab92011087bd1b0b2b5a64f1f97a049ac213f

arch/sim/src/sim/up_initialstate.c
@@ -41,6 +41,9 @@
 
 #include <stdint.h>
 #include <string.h>
+#ifdef CONFIG_SIM_SANITIZE
+#include <sanitizer/asan_interface.h>
+#endif
 
 #include <nuttx/arch.h>
 
@@ -75,4 +78,7 @@ void up_initial_state(struct tcb_s *tcb)
   memset(&tcb->xcp, 0, sizeof(struct xcptcontext));
   tcb->xcp.regs[JB_SP] = (xcpt_reg_t)tcb->adj_stack_ptr - sizeof(xcpt_reg_t);
   tcb->xcp.regs[JB_PC] = (xcpt_reg_t)tcb->start;
+#ifdef CONFIG_SIM_SANITIZE
+  __asan_unpoison_memory_region(tcb->stack_alloc_ptr, tcb->adj_stack_size);
+#endif
 }

ryanoabel/incubator-nuttx

arch/sim/src/sim/up_initialstate.c
@@ -26,6 +26,9 @@
 
 #include <stdint.h>
 #include <string.h>
+#ifdef CONFIG_SIM_ASAN
+#include <sanitizer/asan_interface.h>
+#endif
 
 #include <nuttx/arch.h>
 
@@ -83,4 +86,7 @@ void up_initial_state(struct tcb_s *tcb)
                                      tcb->adj_stack_size -
                                      sizeof(xcpt_reg_t);
   tcb->xcp.regs[JB_PC] = (xcpt_reg_t)tcb->start;
+#ifdef CONFIG_SIM_ASAN
+  __asan_unpoison_memory_region(tcb->stack_alloc_ptr, tcb->adj_stack_size);
+#endif
 }